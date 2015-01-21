cd7774f0d0f29a5c487af7baa63f98a78c2cb352
Marc Mailhot <mmm11105@gmail.com>
2015-01-21 11:57:27 -0500

    Add editing of project titles

--- src/cljs/timetrack/projects.cljs
+++ src/cljs/timetrack/projects.cljs
@@ -9,6 +9,7 @@
   )
 
 (def ENTER_KEY 13)
+(def ESCAPE_KEY 27)
 
 
 
@@ -20,7 +21,31 @@
                               (map :duration)
                               )))
 
+;; Triggers
+(defn- submit [e project owner comm]
+  (when-let [edit-text (om/get-state owner :edit-text)]
+    (if-not (string/blank? (.trim edit-text))
+      (do
+        (om/update! project :name edit-text)
+        (put! comm [:save @project]))
+      (put! comm [:destroy @project])))
+  false)
+(defn- edit [e project owner comm]
+  (let [project @project]
+    (put! comm [:edit project])
+    (om/set-state! owner :edit-text (:name project)))
+  )
 
+(defn- change-title [e project owner]
+  (om/set-state! owner :edit-text (.. e -target -value)))
+
+(defn- edit-keydown [e project owner comm]
+  (condp == (.-keyCode e)
+    ESCAPE_KEY (let [project @project]
+                 (om/set-state! owner :edit-text (:name project))
+                 (put! comm [:cancel project]))
+    ENTER_KEY (submit e project owner comm)
+    nil))
 ;; Handlers
 
 (defn handle-new-project-keydown [e app owner]
@@ -36,28 +61,50 @@
         ))
     false))
 
+(defn edit-project [app {:keys [id] :as project}]
+  (om/update! app :editing id))
+
 (defn destroy-project [app {:keys [id] :as project}]
   (om/transact! app :projects
                 (fn [projects] (into [] (remove #(= (:id %) id) projects)))
                 [:delete id]))
 
+(defn cancel-edit [app {:keys [id]}]
+  (om/update! app :editing nil))
+
+(defn save-project [app {:keys [id]}]
+  (om/update! app :editing nil))
+
 (defn handle-event [type app value]
   (case type
     :destroy (destroy-project app value)
+    :edit (edit-project app value)
+    :save (save-project app value)
+    :cancel (cancel-edit app value)
     nil))
 
 (defcomponent project-summary [project owner]
   (init-state [_]
-              {:edit-text (:title todo)})
+              {:edit-text (:name project)})
   (render-state
    [_ {:keys [comm] :as state}]
-   (dom/li {:class "project-sum-container"}
-           (dom/div {:className "project-sum-header"}
-                    (dom/h3 (:name project)))
-           (dom/div {:className "project-sum-details"}
-                    (dom/p (str "Total Tasks: " (count (:tasks project)) " ")
-                           (str "Total Time: " (format-duration (total-time project)) " ")
-                           (dom/a {:onClick #(put! comm [:destroy @project])} "Delete"))))))
+   (let [class (cond-> "project-sum-container "
+                 (:editing project) (str "editing "))]
+     (dom/li {:className class}
+             (dom/div {:className "view"}
+                      (dom/div {:className "project-sum-header"
+                                :onDoubleClick #(edit % project owner comm)}
+                               (dom/h3 (:name project)))
+                      (dom/div {:className "project-sum-details"}
+                               (dom/p (str "Total Tasks: " (count (:tasks project)) " ")
+                                      (str "Total Time: " (format-duration (total-time project)) " ")
+                                      (dom/a {:onClick #(put! comm [:destroy @project])
+                                              :className "project-sum-delete"} "Delete"))))
+             (dom/input {:ref "editField" :className "project-edit-field"
+                         :value (om/get-state owner :edit-text)
+                         :onChange #(change-title % project owner)
+                         :onKeyDown #(edit-keydown % project owner comm)
+                         :onBlur #(submit % project owner comm)})))))
 
 
 (defcomponent project-page [app owner]
@@ -79,4 +126,7 @@
                                            })
                                (om/build-all project-summary (:projects app)
                                              {:key :id
-                                              :init-state {:comm comm}})))))
+                                              :init-state {:comm comm}
+                                              :fn (fn [project]
+                                                    (cond-> project
+                                                      (= (:id project) (:editing app)) (assoc :editing true)))})))))
